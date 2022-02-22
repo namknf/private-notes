c2bbdb221b59e022ec357957299724ae2ad576d7
anastasiya_malkina <9093264418es@gmail.com>
2022-02-22 19:27:18 +0300

    created model User and db-context UserContext

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateNotes.Models
+{
+    public class User
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+
+        public string Email { get; init; }
+
+        public string Password { get; init; }
+
+        public string PasswordHash { get; init; }
+    }
+}
