14dac2d92c847bd340aa6ced999330629a0867a6
anastasiya_malkina <9093264418es@gmail.com>
2022-02-22 20:35:54 +0300

    created AccountCOntroller for registrtion; installed BCrypt

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using PrivateNotes;
+using PrivateNotes.Pages;
+using BCryptNet = BCrypt.Net.BCrypt;
+
+namespace CustomIdentityApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<Account> _userManager;
+        private readonly SignInManager<Account> _signInManager;
+
+        public AccountController(UserManager<Account> userManager, SignInManager<Account> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Account user = new Account { Email = model.Email, Password = model.Password, RegisterDate = System.DateTime.Now, PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password) };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
