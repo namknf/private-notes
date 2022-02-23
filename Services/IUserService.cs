41a6548aa1ac152511a69b8be4dd326757a3b4d9
anastasiya_malkina <9093264418es@gmail.com>
2022-02-23 15:34:23 +0300

    created interface IUserService

---
+++ Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿namespace PrivateNotes.Services
+{
+    using PrivateNotes.Models;
+    using System.Threading.Tasks;
+
+    public interface IUserService
+    {
+        AuthorizeResponse Authorize(AuthorizeRequest model);
+
+        Task<AuthorizeResponse> Register(Account newUser);
+    }
+}
