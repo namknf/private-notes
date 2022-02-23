67f3c5b96474714fafcab0df84cf7c3940d9eebb
anastasiya_malkina <9093264418es@gmail.com>
2022-02-23 17:30:30 +0300

    added UsersController to connect with front

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿namespace CustomIdentityApp.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using PrivateNotes;
+    using PrivateNotes.Models;
+    using PrivateNotes.Pages;
+    using PrivateNotes.Services;
+    using BCryptNet = BCrypt.Net.BCrypt;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthorizeRequest model)
+        {
+            var response = _userService.Authorize(model);
+
+            if (response == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(Account userModel)
+        {
+            var response = await _userService.Register(userModel);
+
+            if (response == null)
+            {
+                return BadRequest(new { message = "Didn't register!" });
+            }
+
+            return Ok(response);
+        }
+    }
+}
