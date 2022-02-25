dd97463372d1c22f82b432336fe290b39e9d82d4
anastasiya_malkina <9093264418es@gmail.com>
2022-02-25 20:37:46 +0300

    registered services

--- Startup.cs
+++ Startup.cs
@@ -3,10 +3,14 @@ namespace PrivateNotes
     using System;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using PrivateNotes.Helpers;
+    using PrivateNotes.Models;
+    using PrivateNotes.Services;
 
     public class Startup
     {
@@ -25,10 +29,37 @@ namespace PrivateNotes
             var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
             var config2 = Configuration.GetConnectionString("DefaultConnection");
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.RequireUniqueEmail = false;
+            });
+
             // services.AddDbContext<PrivateNotesContext>(options =>
             // options.UseNpgsql(connectionString));
             services.AddDbContext<PrivateNotesContext>(options =>
             options.UseNpgsql(config2));
+
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddScoped(typeof(IUserRepository<>), typeof(UserRepository<>));
+
+            services.AddAutoMapper(typeof(Account));
+            services.AddCors();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,12 +82,18 @@ namespace PrivateNotes
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
             });
+
+            app.UseMiddleware<JwtMiddleware>();
+            app.UseEndpoints(x => x.MapControllers());
         }
     }
 }
