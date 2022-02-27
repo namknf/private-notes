3778352d82b952f3ca7a88807dd40cf366398cfc
anastasiya_malkina <9093264418es@gmail.com>
2022-02-27 13:05:21 +0300

    removed logger and OnGet()

--- Pages/RegistrationModel.cshtml.cs
+++ Pages/RegistrationModel.cshtml.cs
@@ -1,18 +1,10 @@
 namespace PrivateNotes.Pages
 {
     using System.ComponentModel.DataAnnotations;
-    using Microsoft.Extensions.Logging;
     using PrivateNotes.Models;
 
     public class RegistrationModel : BaseModel
     {
-        private readonly ILogger<RegistrationModel> _logger;
-
-        public RegistrationModel(ILogger<RegistrationModel> logger)
-        {
-            _logger = logger;
-        }
-
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
@@ -28,9 +20,5 @@ namespace PrivateNotes.Pages
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
         public string PasswordConfirm { get; set; }
-
-        public void OnGet()
-        {
-        }
     }
 }
