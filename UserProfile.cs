1a0051a7e1ced530363a425f8d9882f979ca82d0
anastasiya_malkina <9093264418es@gmail.com>
2022-02-27 16:26:24 +0300

    created new maps

--- UserProfile.cs
+++ UserProfile.cs
@@ -2,22 +2,27 @@
 {
     using AutoMapper;
     using PrivateNotes.Models;
+    using PrivateNotes.Pages;
 
     public class UserProfile : Profile
     {
         public UserProfile()
         {
-            CreateMap<User, Account>()
+            CreateMap<User, RegistrationModel>()
                 .ForMember(dst => dst.Email, opt => opt.MapFrom(src => src.Email))
-                .ForMember(dst => dst.PasswordHash, opt => opt.MapFrom(src => src.PasswordHash))
-                .ForMember(dst => dst.RegisterDate, opt => opt.MapFrom(src => src.RegisterDate))
+                .ForMember(dst => dst.Password, opt => opt.MapFrom(src => src.Password))
+                .ForMember(dst => dst.Id, opt => opt.Ignore())
+                ;
+
+            CreateMap<RegistrationModel, User>()
+                .ForMember(dst => dst.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dst => dst.Password, opt => opt.MapFrom(src => src.Password))
                 .ForMember(dst => dst.Id, opt => opt.Ignore())
                 ;
 
             CreateMap<User, AuthorizeResponse>()
                 .ForMember(dst => dst.Email, opt => opt.MapFrom(src => src.Email))
                 .ForMember(dst => dst.PasswordHash, opt => opt.MapFrom(src => src.PasswordHash))
-                .ForMember(dst => dst.RegisterDate, opt => opt.MapFrom(src => src.RegisterDate))
                 .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dst => dst.Notes, opt => opt.MapFrom(src => src.Notes))
                 .ForMember(dst => dst.Token, opt => opt.MapFrom(src => src.Token))
