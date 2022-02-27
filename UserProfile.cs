943c363f1366d147bb6ed4f753608afada4f04b8
anastasiya_malkina <9093264418es@gmail.com>
2022-02-27 10:16:14 +0300

    created maps

--- UserProfile.cs
+++ UserProfile.cs
@@ -1,6 +1,27 @@
 ﻿namespace PrivateNotes
 {
-    public class UserProfile
+    using AutoMapper;
+    using PrivateNotes.Models;
+
+    public class UserProfile : Profile
     {
+        public UserProfile()
+        {
+            CreateMap<User, Account>()
+                .ForMember(dst => dst.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dst => dst.PasswordHash, opt => opt.MapFrom(src => src.PasswordHash))
+                .ForMember(dst => dst.RegisterDate, opt => opt.MapFrom(src => src.RegisterDate))
+                .ForMember(dst => dst.Id, opt => opt.Ignore())
+                ;
+
+            CreateMap<User, AuthorizeResponse>()
+                .ForMember(dst => dst.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dst => dst.PasswordHash, opt => opt.MapFrom(src => src.PasswordHash))
+                .ForMember(dst => dst.RegisterDate, opt => opt.MapFrom(src => src.RegisterDate))
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.Notes, opt => opt.MapFrom(src => src.Notes))
+                .ForMember(dst => dst.Token, opt => opt.MapFrom(src => src.Token))
+                ;
+        }
     }
 }
