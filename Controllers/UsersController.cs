b3c805075b438839a94892ef9fb95dacb7580996
anastasiya_malkina <9093264418es@gmail.com>
2022-03-01 21:20:05 +0300

    added new redirects

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,11 +1,9 @@
-﻿namespace CustomIdentityApp.Controllers
+﻿namespace PrivateNotes.Controllers
 {
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
-    using PrivateNotes.Models;
     using PrivateNotes.Pages;
     using PrivateNotes.Services;
-    using BCryptNet = BCrypt.Net.BCrypt;
 
     [ApiController]
     [Route("Users")]
@@ -18,13 +16,8 @@
             _userService = userService;
         }
 
-        public IActionResult Registration()
-        {
-            return View();
-        }
-
         [HttpPost("authenticate")]
-        public IActionResult Authenticate(AuthorizationModel model)
+        public IActionResult Authenticate([FromForm] AuthorizationModel model)
         {
             var response = _userService.Authenticate(model);
 
@@ -33,20 +26,20 @@
                 return BadRequest(new { message = "Username or password is incorrect" });
             }
 
-            return Ok(response);
+            return Redirect("~/");
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(RegistrationModel userModel)
+        public async Task<IActionResult> Register([FromForm] RegistrationModel userModel)
         {
             var response = await _userService.Register(userModel);
 
             if (response == null)
             {
-                return BadRequest(new { message = "Didn't register!" });
+                return BadRequest(new {message = "Didn't register!"});
             }
 
-            return Ok(response);
+            return Redirect("~/");
         }
     }
 }
