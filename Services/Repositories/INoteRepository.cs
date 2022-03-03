0d4675076dca537ad5eb277a4bece12388f109e3
anastasiya_malkina <9093264418es@gmail.com>
2022-03-03 08:25:30 +0300

    modified interface INoteRepository, implemented it in class NoteRepository

--- Services/Repositories/INoteRepository.cs
+++ Services/Repositories/INoteRepository.cs
@@ -1,6 +1,14 @@
 ﻿namespace PrivateNotes.Services.Repositories
 {
-    public class INoteRepository
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    internal interface INoteRepository<T>
     {
+        T GetNoteById(int id);
+
+        Task<int> Add(T entity);
+
+        List<T> GetAll();
     }
 }
--- Services/Repositories/NoteRepository.cs
+++ Services/Repositories/NoteRepository.cs
@@ -1,6 +1,39 @@
 ﻿namespace PrivateNotes.Services.Repositories
 {
-    public class NoteRepository
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using PrivateNotes.Models;
+
+    public class NoteRepository<T> : INoteRepository<T>
+        where T : BaseModel
     {
+        private readonly PrivateNotesContext _context;
+
+        public NoteRepository(PrivateNotesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Add(T entity)
+        {
+            var result = await _context.Set<T>().AddAsync(entity);
+
+            await _context.SaveChangesAsync();
+
+            return result.Entity.Id;
+        }
+
+        public T GetNoteById(int id)
+        {
+            var result = _context.Set<T>().FirstOrDefault(x => x.Id == id);
+
+            return result;
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
     }
 }
