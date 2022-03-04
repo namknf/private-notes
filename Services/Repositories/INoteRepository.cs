6054357fe806b7812bea4848f130fb06f66ae1d4
anastasiya_malkina <9093264418es@gmail.com>
2022-03-04 08:56:57 +0300

    modified access modifier

--- Services/Repositories/INoteRepository.cs
+++ Services/Repositories/INoteRepository.cs
@@ -3,7 +3,7 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
-    internal interface INoteRepository<T>
+    public interface INoteRepository<T>
     {
         T GetNoteById(int id);
 
