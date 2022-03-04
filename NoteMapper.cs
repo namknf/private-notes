94a4dc674c84db06cd043e00a04ed4ef48f1e166
anastasiya_malkina <9093264418es@gmail.com>
2022-03-04 19:12:29 +0300

    reverse mapping

--- NoteMapper.cs
+++ NoteMapper.cs
@@ -14,6 +14,14 @@
                 .ForMember(dst => dst.NoteUser, opt => opt.MapFrom(src => src.NoteUser))
                 .ForMember(dst => dst.Id, opt => opt.Ignore())
                 ;
+
+            CreateMap<NoteResponse, Note>()
+                .ForMember(dst => dst.Text, opt => opt.MapFrom(src => src.Text))
+                .ForMember(dst => dst.NoteDate, opt => opt.MapFrom(src => src.NoteDate))
+                .ForMember(dst => dst.NoteUserId, opt => opt.MapFrom(src => src.NoteUserId))
+                .ForMember(dst => dst.NoteUser, opt => opt.MapFrom(src => src.NoteUser))
+                .ForMember(dst => dst.Id, opt => opt.Ignore())
+                ;
         }
     }
 }
