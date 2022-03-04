e7e776e9fc7cfc12b9aad319c41c6b38ea9923d8
anastasiya_malkina <9093264418es@gmail.com>
2022-03-04 19:11:08 +0300

    implemented mapping

--- NoteMapper.cs
+++ NoteMapper.cs
@@ -1,6 +1,19 @@
 ﻿namespace PrivateNotes
 {
-    public class NoteMapper
+    using AutoMapper;
+    using PrivateNotes.Models;
+
+    public class NoteMapper : Profile
     {
+        public NoteMapper()
+        {
+            CreateMap<Note, NoteResponse>()
+                .ForMember(dst => dst.Text, opt => opt.MapFrom(src => src.Text))
+                .ForMember(dst => dst.NoteDate, opt => opt.MapFrom(src => src.NoteDate))
+                .ForMember(dst => dst.NoteUserId, opt => opt.MapFrom(src => src.NoteUserId))
+                .ForMember(dst => dst.NoteUser, opt => opt.MapFrom(src => src.NoteUser))
+                .ForMember(dst => dst.Id, opt => opt.Ignore())
+                ;
+        }
     }
 }
