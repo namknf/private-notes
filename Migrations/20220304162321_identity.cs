cdfb14898f147f04a0cd60bed5e0c33209487f2f
anastasiya_malkina <9093264418es@gmail.com>
2022-03-04 19:30:39 +0300

    created new migrations

---
+++ Migrations/20220304162321_identity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PrivateNotes.Migrations
+{
+    public partial class identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accounts");
+
+            migrationBuilder.DropTable(
+                name: "notes");
+
+            migrationBuilder.DropColumn(
+                name: "RegisterDate",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "Token");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Token",
+                table: "Users",
+                newName: "PasswordHash");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegisterDate",
+                table: "Users",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    password = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: false),
+                    password_hash = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    register_date = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("accounts_pkey", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "notes",
+                columns: table => new
+                {
+                    note_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    note_date = table.Column<DateTime>(type: "date", nullable: false),
+                    text = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_notes", x => x.note_id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "accounts_email_key",
+                table: "accounts",
+                column: "email",
+                unique: true);
+        }
+    }
+}
