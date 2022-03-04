83a63f6bcdeaa784fc8882cc159071324ff6f002
anastasiya_malkina <9093264418es@gmail.com>
2022-03-04 08:53:36 +0300

    inherits from BaseModel

--- Models/Note.cs
+++ Models/Note.cs
@@ -7,10 +7,8 @@ namespace PrivateNotes.Models
     /// <summary>
     /// Note1 model.
     /// </summary>
-    public partial class Note
+    public partial class Note : BaseModel
     {
-        public int Id { get; set; }
-
         public DateTime NoteDate { get; set; }
 
         public string Text { get; set; }
