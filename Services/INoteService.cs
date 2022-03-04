f395974688ae6b273f555a83588b091c4cf98478
anastasiya_malkina <9093264418es@gmail.com>
2022-03-04 08:57:30 +0300

    implemented interface in NoteService

--- Services/INoteService.cs
+++ Services/INoteService.cs
@@ -1,6 +1,11 @@
 ﻿namespace PrivateNotes.Services
 {
+    using System.Threading.Tasks;
+    using PrivateNotes.Models;
+    using PrivateNotes.Pages;
+
     public interface INoteService
     {
+        Task<NoteResponse> Create(CreateNoteModel note, User user);
     }
 }
--- Services/NoteService.cs
+++ Services/NoteService.cs
@@ -1,6 +1,37 @@
 ﻿namespace PrivateNotes.Services
 {
+    using System;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Configuration;
+    using PrivateNotes.Models;
+    using PrivateNotes.Pages;
+    using PrivateNotes.Services.Repositories;
+
     public class NoteService : INoteService
     {
+        private readonly INoteRepository<Note> _noteRepository;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+        private UserManager<User> _userManager;
+
+        public NoteService(INoteRepository<Note> noteRepository, IConfiguration configuration, IMapper mapper)
+        {
+            _noteRepository = noteRepository;
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        public async Task<NoteResponse> Create(CreateNoteModel model, User user)
+        {
+            var note = _mapper.Map<Note>(model);
+
+            var addedUser = await _noteRepository.Add(note);
+
+            var response = new NoteResponse(user, model);
+
+            return response;
+        }
     }
 }
