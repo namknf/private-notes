89fc44d12adfe768ff900f13c7976a508e411333
anastasiya_malkina <9093264418es@gmail.com>
2022-03-09 19:20:13 +0300

    inherits from IdentityUser; added class AppRole

--- Models/User.cs
+++ Models/User.cs
@@ -3,23 +3,28 @@
 namespace PrivateNotes.Models
 {
     using System.Collections.Generic;
+    using Microsoft.AspNetCore.Identity;
 
     /// <summary>
     /// User model.
     /// </summary>
-    public partial class User : BaseModel
+    public sealed partial class User : IdentityUser<int>
     {
         public User()
         {
             Notes = new HashSet<Note>();
         }
 
-        public string Email { get; set; }
+        public override string Email { get; set; }
 
         public string Password { get; set; }
 
         public string Token { get; set; }
 
-        public virtual ICollection<Note> Notes { get; set; }
+        public ICollection<Note> Notes { get; set; }
+    }
+
+    public class AppRole : IdentityRole<int>
+    {
     }
 }
