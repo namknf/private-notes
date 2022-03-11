5bb8166698b32803b7140744f308f78af116cfee
anastasiya_malkina <9093264418es@gmail.com>
2022-03-11 19:36:35 +0300

    fixed hashing

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,5 +1,7 @@
 ﻿namespace PrivateNotes.Services
 {
+    using BCryptNet = BCrypt.Net.BCrypt;
+    using System;
     using System.Linq;
     using System.Threading.Tasks;
     using AutoMapper;
@@ -11,15 +13,17 @@
 
     internal class UserService : IUserService
     {
+        private readonly PrivateNotesContext _context;
         private readonly IUserRepository<User> _userRepository;
         private readonly IConfiguration _configuration;
         private readonly IMapper _mapper;
 
-        public UserService(IUserRepository<User> userRepository, IConfiguration configuration, IMapper mapper)
+        public UserService(IUserRepository<User> userRepository, IConfiguration configuration, IMapper mapper, PrivateNotesContext context)
         {
             _userRepository = userRepository;
             _configuration = configuration;
             _mapper = mapper;
+            _context = context;
         }
 
         public AuthorizeResponse Authenticate(AuthorizationModel model)
@@ -30,9 +34,9 @@
 
             if (user != null)
             {
-                user.Password = BCrypt.Net.BCrypt.HashPassword(model.Password);
+                user.Password = model.Password;
 
-                var isValidPassword = BCrypt.Net.BCrypt.Verify(model.Password, user.Password);
+                var isValidPassword = BCrypt.Net.BCrypt.Verify(model.Password, user.PasswordHash);
 
                 if (isValidPassword)
                 {
@@ -49,12 +53,19 @@
         {
             var user = _mapper.Map<User>(userModel);
 
+            if (_context.Users.Any(x => x.Email == userModel.Email))
+            {
+                throw new ArgumentException("Email '" + userModel.Email + "' is already taken");
+            }
+
+            user.PasswordHash = BCryptNet.HashPassword(userModel.Password);
+
             var addedUser = await _userRepository.Add(user);
 
             var response = Authenticate(new AuthorizationModel
             {
                 Email = user.Email,
-                Password = BCrypt.Net.BCrypt.HashPassword(user.Password),
+                Password = user.Password,
             });
 
             return response;
