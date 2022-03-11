5d631071f6d370abc0c1ad85fb16a44090ac00ad
anastasiya_malkina <9093264418es@gmail.com>
2022-03-11 19:38:16 +0300

    added claims

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,10 +2,15 @@
 {
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
     using PrivateNotes.Pages;
     using PrivateNotes.Services;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using Microsoft.AspNetCore.Authentication;
 
     [ApiController]
+    [Authorize]
     [Route("users")]
     public class UsersController : Controller
     {
@@ -16,7 +21,14 @@
             _userService = userService;
         }
 
+        [AllowAnonymous]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
         [HttpPost("authenticate")]
+        [AllowAnonymous]
         public IActionResult Authenticate([FromForm] AuthorizationModel model)
         {
             var response = _userService.Authenticate(model);
@@ -30,6 +42,7 @@
         }
 
         [HttpPost("register")]
+        [AllowAnonymous]
         public async Task<IActionResult> Register([FromForm] RegistrationModel userModel)
         {
             var response = await _userService.Register(userModel);
@@ -39,6 +52,23 @@
                 return BadRequest(new { message = "Didn't register!" });
             }
 
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,userModel.Email),
+                new Claim(ClaimTypes.Role,"Administrator")
+            };
+
+            var claimIdentity = new ClaimsIdentity(claims, "Cookie");
+            var claimPrincipal = new ClaimsPrincipal(claimIdentity);
+            await HttpContext.SignInAsync("Cookie", claimPrincipal);
+
+            return Redirect("~/");
+        }
+
+        [HttpPost("logout")]
+        public IActionResult LogOut()
+        {
+            HttpContext.SignOutAsync("Cookie");
             return Redirect("~/");
         }
     }
